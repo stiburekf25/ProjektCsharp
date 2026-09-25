0c2e1e254874a37b2b0f83fbbcaa1e49e833b18c
stiburekf <stiburekf25@spstrutnov.cz>
2026-09-25 12:06:37 +0200

    vymazaní obj a bin

--- ProjektC#/Kamen_Nuzky_Papir/obj/Debug/net10.0/Kamen_Nuzky_Papir.AssemblyInfo.cs
+++ ProjektC#/Kamen_Nuzky_Papir/obj/Debug/net10.0/Kamen_Nuzky_Papir.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kamen_Nuzky_Papir")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f20003489c8afdfce501aec0123285abbbf166cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kamen_Nuzky_Papir")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kamen_Nuzky_Papir")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ProjektC#/ProjektC#/Papousek.cs
+++ ProjektC#/ProjektC#/Papousek.cs
@@ -24,6 +24,7 @@
                 if (CoUzivatelNapsal == "Franta smrdi")
                 {
                     Console.WriteLine("ne, je to nadherný chlapec");
+                    ahoj;
                 }
             }
             
