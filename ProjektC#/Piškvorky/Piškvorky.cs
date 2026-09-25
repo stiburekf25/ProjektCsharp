caf4002954b40c744d9639a0a6a82e6135756d20
stiburekf <stiburekf25@spstrutnov.cz>
2026-09-25 12:14:35 +0200

    Piškvorky_soubor

--- ProjektC#/Piškvorky/Piškvorky.cs
+++ ProjektC#/Piškvorky/Piškvorky.cs
@@ -1,10 +1,15 @@
 ﻿namespace Piškvorky
 {
-    internal class Program
+    internal class Piškvorky
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Vítej v piškvorkách, vyber si X nebo O");
+            string UzivatelVyber = Console.ReadLine().ToLower;
+
+            if 
+
+
 
         }
     }
